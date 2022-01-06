49127eede2f3e5294dac7c8ca4c05a10fb387eba
dougc333 <dougc@stanfordalumni.org>
2022-01-06 14:11:06 -0800

    add

---
+++ test_globals.py
@@ -0,0 +1,13 @@
+class A:
+  def __init__(self):
+     self.A=10
+     import pandas as pd
+  #print(globals())
+
+a=A()
+import numpy as np
+#b=np.array([1,2,3,4])
+
+print("globals:")
+print(globals())
+
