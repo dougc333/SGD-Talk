f26b5c769bf748be8f87b11c407c983dfaff22d2
doug chang <dougc@stanfordalumni.org>
2022-02-02 13:25:40 -0800

    add

---
+++ typescript/first/test.js
@@ -0,0 +1,6 @@
+"use strict";
+const i1 = "4";
+const i2 = "2";
+function add(i1, i2) {
+    console.log(i1 + i2);
+}
---
+++ typescript/first/test.ts
@@ -0,0 +1,8 @@
+
+const i1 = "4"
+const i2 = "2"
+
+function add(i1:string,i2:string){
+  console.log(i1+i2)
+  
+}
