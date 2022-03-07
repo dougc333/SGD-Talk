291541f78dcdb1ce86a5f4b33662f0032a96ae25
doug chang <dougc@stanfordalumni.org>
2022-03-07 08:51:46 -0800

    add

--- cosmic/ch1/ex1.py
+++ cosmic/ch1/ex1.py
@@ -3,22 +3,50 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional
 import itertools
+import pytest
+
+
+# some design problems:
+# 1) autoincrement the id, if you autoincrement, save last known for startup
+# 2) correlate w/db. have db create pk then read from db
+# 3) integrity check on id, if you create it make sure there are no holes
+# else DB responsibility. Built into db autoincrement index. Guaranteed to be atomic
+# thread safe and fault tolerant in DB. 
+# 4) microservice, need lock for shared state. db has locks, you dont see them
+# 
+
 #not frozen
 class Batch:
-    def __init__(self, batch_id:str, iName:str, qty:int, eta:Optional[date]):
-        id:batch_id
+    def __init__(self, batch_id:str, sku:str, qty:int, eta:Optional[date]):
+        reference_id:batch_id
         self.batchName=batch_id
-        self.sku =iName
+        self.sku =sku;
         self.available_quantity=qty
-        eta = eta
+        self.eta = eta
+        self.allocated_orderlines=[]
+        
     def allocate(self,orderLine):
-        self.available_quantity -=orderLine.qty
+        if self.can_allocate(orderLine)==True:
+            self.available_quantity -=orderLine.qty
+            self.allocated_orderlines.append(orderLine)
+        
     def can_allocate(self,orderLine):
-        #not sure what this does? compares teh 2 and returns boolean
-        if orderLine.qty>self.available_quantity or orderLine.sku!=self.sku:
+        if orderLine.qty>self.available_quantity or orderLine.sku!=self.sku or self.search(orderLine)==True:
             return False
         return True
-
+    
+    def deallocate(orderLine):
+        #search for allocated orderLines and deallocate it
+        #this isnt the correct method we need a search orderLine to return True or False
+        print("deallocate orderLine")
+        
+    def search(self,orderLine):
+        for x in self.allocated_orderlines:
+            if x==orderLine:
+                return True
+        return False
+        
+        
 class Product:
     sku:str
 
@@ -42,9 +70,6 @@ class Order:
         self.order_ref:str
         self.order_reference:int
         self.orderLines:list[OrderLine]
-
- 
-
 
 def make_batch_and_line(sku, batch_qty, line_qty):
     return (
@@ -77,9 +102,74 @@ def test_cannot_allocate_if_skus_do_not_match():
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
 
+def test_search_orderline():
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    ol1 = OrderLine("order_test1","UNCOMFORTABLE-CHAIR", 1)
+    batch.allocate(ol1)
+    assert batch.search(ol1) is True
+#this is kind of a funny interface. Batch is created by someone which orders inventory. 
+#
+#it is really a set? all of these are unique? Doesnt make sense
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+    
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+
+def test_prefers_earlier_batches():
+    earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
+    medium = Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
+    latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
+    line = OrderLine("order1", "MINIMALIST-SPOON", 10)
+
+    allocate(line, [medium, earliest, latest])
+
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100
+
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, eta=None)
+    shipment_batch = Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, eta=tomorrow)
+    line = OrderLine("oref", "HIGHBROW-POSTER", 10)
+    allocation = allocate(line, [in_stock_batch, shipment_batch])
+    assert allocation == in_stock_batch.reference
+
+    
+from datetime import timedelta
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
+
+
+
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    print("I dont know whwat this is")
+
+
+def test_prefers_earlier_batches():
+    print("dont know thwa this iss")
+
+
+
 test_allocating_to_a_batch_reduces_the_available_quantity()
 test_can_allocate_if_available_greater_than_required()
 test_cannot_allocate_if_available_smaller_than_required()
 test_can_allocate_if_available_equal_to_required()
 test_cannot_allocate_if_skus_do_not_match()
-
+test_search_orderline()
+test_allocation_is_idempotent()
