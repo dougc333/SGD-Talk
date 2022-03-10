869b915a06327cb921e52d05c037e5c185396df6
doug chang <dougc@stanfordalumni.org>
2022-03-10 10:42:42 -0800

    add

--- cosmic/ch1/test_mypy_bamed_tuple.py
+++ cosmic/ch1/test_mypy_bamed_tuple.py
@@ -14,6 +14,20 @@ class B(NamedTuple):
 print("this should be False:",A("aa")==B('aa'))
 print(sorted([A("bb"),B("aa"),A("aa")]))
 
+
+#__dict__ stores the object's writable attributes 
+
+class SomeObject:
+    def __init__(self, name:str)->None:
+        self.name=name
+s = SomeObject("SomeObject")
+print("s.name:",s.name)
+s.name="asdf"
+print("s.name:",s.name)
+print("__dict__:",s.__dict__)
+        
+
+
 class User:
     def __init__(self,id:UUID,username:str)->None:
         self.id=id
@@ -26,4 +40,6 @@ u1 = User(uuid.uuid4(), "bob")
 u2 = User(uuid.uuid4(), "ann")
 print("u1==u2 should be False:",u2==u2)
 print(dir(u1))
+a = u1.from_dict({'id':"",'username':""})
+print("a:",a)
 
