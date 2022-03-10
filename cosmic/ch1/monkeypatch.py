4eed2c25b8b4e4972c6065ee049b7427feb56f67
doug chang <dougc@stanfordalumni.org>
2022-03-10 11:53:54 -0800

    add

---
+++ cosmic/ch1/monkeypatch.py
@@ -0,0 +1,5 @@
+#monkeypatching is dynamic replacement of code
+#this can be useful for mocking or testing if the real source isnt avaiable like a URL request
+
+
+https://stackoverflow.com/questions/5626193/what-is-monkey-patching
