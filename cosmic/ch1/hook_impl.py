5eaea872da05e27453f146fb0ad39b4f45022652
dougc333 <dougc@stanfordalumni.org>
2022-04-10 13:01:41 -0700

    add

--- cosmic/ch1/hook_impl.py
+++
@@ -1,5 +0,0 @@
-https://stackoverflow.com/questions/4309607/whats-the-preferred-way-to-implement-a-hook-or-callback-in-python
-
-implementing a hook. 
-when is this good in the api? 
-adding hook interface
--- cosmic/ch1/monkeypatch.py
+++
@@ -1,8 +0,0 @@
-#monkeypatching is dynamic replacement of code
-#this can be useful for mocking or testing if the real source isnt avaiable like a URL request
-
-#monkeypatching means changing class/object when running, inside the runtime
-#only works because everything in python is mutable. 
-https://stackoverflow.com/questions/5626193/what-is-monkey-patching
-
-#test can dataclass be monkeypatched? 
