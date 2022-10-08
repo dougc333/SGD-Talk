ad3e401275b934a69f2545572f891e37d95dae25
doug chang <dougc@stanfordalumni.org>
2022-10-08 16:04:48 -0700

    add

--- flask/forms/prepopulate_data/app.py
+++ flask/forms/prepopulate_data/app.py
@@ -1,10 +1,13 @@
 from ast import Pass
+from tokenize import String
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from numpy import character
 from pytest import PytestAssertRewriteWarning
 from wtforms import StringField, IntegerField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length, AnyOf, Email
 
+from dataclasses import dataclass
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -24,7 +27,14 @@ class User:
         self.username=username
         self.age = age
         self.email = email
+#try replacing with dataclass
+@dataclass
+class UserDataClass:
+    name: str
+    gender: str
+
     
+
 
 def create_app():
     app = Flask(__name__)
@@ -33,7 +43,7 @@ def create_app():
     app.config['SECRET_KEY']= 'asdfasdfasdf'
     
     #we have to add the routes in here 
-    @app.route("/")
+    @app.route("/", methods=['GET','POST'])
     def login():
         user = User(username="bob", age=100, email="foo@foo.com")
         form = LoginForm(obj=user)
