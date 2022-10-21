bc9cec8049bbe95a2743541048c6e49cda99e9e2
doug chang <dougc@stanfordalumni.org>
2022-10-21 12:13:06 -0700

    add

--- bandits/README.md
+++
@@ -1,2 +0,0 @@
-https://eugeneyan.com/writing/bandits/
-
--- test_3server/test.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-#how to submit code to aws to run and get results back
-def create_app():
-    app = Flask(__name__)
-    return app
