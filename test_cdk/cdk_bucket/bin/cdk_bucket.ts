89f12ddc2c98b5a44bff922d8a134e677781725f
doug chang <dougc@stanfordalumni.org>
2022-10-26 08:18:29 -0700

    add

--- test_cdk/cdk_bucket/bin/cdk_bucket.ts
+++ test_cdk/cdk_bucket/bin/cdk_bucket.ts
@@ -4,6 +4,11 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkBucketStack } from '../lib/cdk_bucket-stack';
 
 const app = new cdk.App();
+//need to define this to get s3Deploy to upload files
+const myEnv = {
+  account:'669059827483',
+  region:'us-west-2',
+}
 new CdkBucketStack(app, 'CdkBucketStack', {
-  
+  env:myEnv
 });
