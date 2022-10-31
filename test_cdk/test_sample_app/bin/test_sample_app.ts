7372e2149379fbcadb3793833d0e038ac4162eb5
doug chang <dougc@stanfordalumni.org>
2022-10-31 15:41:08 -0700

    add

--- test_cdk/test_sample_app/bin/test_sample_app.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { TestSampleAppStack } from '../lib/test_sample_app-stack';
-
-const app = new cdk.App();
-new TestSampleAppStack(app, 'TestSampleAppStack');
