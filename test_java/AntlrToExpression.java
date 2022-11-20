910a173570b37929b3f74fe6322323c59dae5159
dougc333 <dougc@stanfordalumi.org>
2022-11-20 06:44:12 -0800

    antlr

--- test_java/AntlrToExpression.java
+++ test_java/AntlrToExpression.java
@@ -1,33 +1,79 @@
+import java.util.List;
+import org.antlr.v4.runtime.Token;
+import java.util.ArrayList;
+
 public class AntlrToExpression extends ExprBaseVisitor<Expression> {
+    /*
+     * Given that all visit_ methods are called topdown, we can be sure
+     * the order which we addd declared variables into vars is 
+     * identical to the order in the source code file
+     * 
+     */
+    private List<String> vars;
+    private List<String> semanticErrors; /*DUPLICATE DECL    OR REFERENCE TO UNDECLARED IDENTIFIER, ADD MISSING DEFAULT PROPERRT PULL IT OFF A CRAWLER */
+
+    public AntlrToExpression(List<String> semanticErrors){
+        List<String> vars = new ArrayList<String>();
+        this.semanticErrors = semanticErrors;
+    }
 
     @Override
     public Expression visitAddition(ExprParser.AdditionContext ctx) {
-        // TODO Auto-generated method stub
-        return super.visitAddition(ctx);
+
+        Expression left = visit(ctx.getChild(0));
+        Expression right = visit(ctx.getChild(2));
+        return new Addition(left,right);
     }
 
     @Override
     public Expression visitDeclaration(ExprParser.DeclarationContext ctx) {
-        // TODO Auto-generated method stub
-        return super.visitDeclaration(ctx);
+        Token idToken = ctx.ID().getSymbol();
+        int line = idToken.getLine();
+        int column = idToken.getCharPositionInLine()+1;
+        
+        String id = ctx.getChild(0).getText();
+        if (vars.contains(id)){
+            semanticErrors.add("Error variable"+id+"alreadu del;cared");
+        }else{
+            vars.add(id);
+        }
+        String type = ctx.getChild(2).getText();
+        int value = Integer.parseInt(ctx.NUM().getText());
+
+        return new VariableDeclaration(id,type,value);
+
+
     }
 
     @Override
     public Expression visitMultiplication(ExprParser.MultiplicationContext ctx) {
-        // TODO Auto-generated method stub
-        return super.visitMultiplication(ctx);
+        
+        Expression left = visit(ctx.getChild(0));
+        Expression right = visit(ctx.getChild(2));
+        return new Multiplication(left,right);
+    
+    
     }
 
     @Override
     public Expression visitNumber(ExprParser.NumberContext ctx) {
         // TODO Auto-generated method stub
-        return super.visitNumber(ctx);
+        String numText = ctx.getChild(0).getText();
+        int num = Integer.parseInt(numText);
+        return new Number(num);
     }
 
     @Override
     public Expression visitVariable(ExprParser.VariableContext ctx) {
-        // TODO Auto-generated method stub
-        return super.visitVariable(ctx);
+        
+        Token idToken = ctx.ID().getSymbol();
+        int line = idToken.getLine();
+        int column = idToken.getCharPositionInLine()+1;
+        String id = ctx.getChild(0).getText();
+        if (!vars.contains(id)){
+            semanticErrors.add("error variable"+id+" not declared");
+        }
+        return new Variable(id);
     }
     
 }
---
+++ test_java/AntlrToProgram.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AntlrToProgram extends ExprBaseVisitor<Program>{
+    public List<String> semanticErrors;
+
+    @Override
+    public Program visitProgram(ExprParser.ProgramContext ctx) {
+        // TODO Auto-generated method stub
+        Program prog = new Program();
+        List<String> semanticErrors = new ArrayList<String>();
+        AntlrToExpression exprVisitor = new AntlrToExpression(semanticErrors);
+        for (int i = 0; i < ctx.getChildCount() -1; i++ ){
+            if(i == ctx.getChildCount()-1){
+                //do nothing we do not want to visit this child and attempt 
+                //to convert it to an expression object
+                
+            }else{
+                prog.addExpression(exprVisitor.visit(ctx.getChild(i)));
+            }
+
+        }
+        return prog;
+     }
+
+    
+}
