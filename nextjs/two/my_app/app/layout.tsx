b3500f40e91fd3fc3fa1e14274f3057f92abf216
dougc333 <dougc@stanfordalumni.org>
2022-12-13 09:46:40 -0800

    add

---
+++ nextjs/two/my_app/app/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <main>
+          <nav>
+          <Link href="/">
+             Home
+          </Link>
+          <Link href="/notes">
+             Notes
+          </Link>
+          </nav>
+        {children}
+        </main>
+       </body>
+    </html>
+  )
+}
---
+++ nextjs/two/my_app/pb_migrations/1670916631_created_posts1.js
@@ -0,0 +1,38 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "ohlxcaac4ny6a4m",
+    "created": "2022-12-13 07:30:31.538Z",
+    "updated": "2022-12-13 07:30:31.538Z",
+    "name": "posts1",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "rzksblav",
+        "name": "name",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("ohlxcaac4ny6a4m");
+
+  return dao.deleteCollection(collection);
+})
