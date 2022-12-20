ca144e003c0054bbc0d1652af5d230eeeca7cab0
dougc333 <dougc@stanfordalumni.org>
2022-12-20 08:52:22 -0800

    add

---
+++ python/ml_server/second/api.py
@@ -0,0 +1,9 @@
+import request
+import response
+
+class API
+  def __init__(self):
+     debug_string="general API for crawling"
+     start+_url="http://www.stanford.edu"
+  def crawl_one_level(self):
+     
