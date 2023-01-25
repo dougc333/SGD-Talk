ca0d16052130d95d2dad8c9e8225e5fdd29fa695
dougc333 <dougc@stanfordalumni.org>
2023-01-25 11:02:49 -0800

    add

---
+++ react_proj/projects/p1.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class P1 extends React.Component{
+    super()
+    
+    render(){
+        <div>"react component here</div>
+    }
+}
+
+
+ReactDOM.render(
+    <P1></P1>,
+    document.getElementById("root")
+)
