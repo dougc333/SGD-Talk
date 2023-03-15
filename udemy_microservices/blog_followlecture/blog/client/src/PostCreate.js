05c6ab3751412678e9c810a52eb90e5d447ab453
doug chang <dougc@stanfordalumni.org>
2023-03-15 13:38:10 -0700

    add

--- udemy_microservices/blog_followlecture/blog/client/src/PostCreate.js
+++ udemy_microservices/blog_followlecture/blog/client/src/PostCreate.js
@@ -8,7 +8,7 @@ const PostCreate=()=>{
 
     const onSubmit=async(e)=>{
         e.preventDefault()
-        await axios.post('http://localhost:4000',{
+        await axios.post('http://localhost:4000/posts',{
             title
         })
         setTitle('')
