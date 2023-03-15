7df37f63d11c5f01f4a57049722bbc2474bd4238
doug chang <dougc@stanfordalumni.org>
2023-03-15 13:30:37 -0700

    add

--- udemy_microservices/blog_followlecture/blog/comments/index.js
+++ udemy_microservices/blog_followlecture/blog/comments/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('bodyParser')
 const {randomBytes} = require('crypto')
-
+const cors = require('cors')
 
 const app = express()
+
 app.use(bodyParser.json())
+app.use(cors())
 
 const commentsByPostId = {}
 
--- udemy_microservices/blog_followlecture/blog/posts/index.js
+++ udemy_microservices/blog_followlecture/blog/posts/index.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const {randomBytes} = require('crypto')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {}
+const posts = {};
 
-app.get('/posts', (req,res)=>{
-    res.send(posts)    
-})
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
-app.post('/posts',(req,res)=>{
-    const id = randomBytes(4).toStrong('hex')
-    const {title} = req.body
-    posts[id] = {
-        id,
-        title
-    }
-    res.status(201).send(posts[id])
-})
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-app.listen(4000,()=>{
-    console.log("listening on localhost:4000")
-})
+  posts[id] = {
+    id,
+    title
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
