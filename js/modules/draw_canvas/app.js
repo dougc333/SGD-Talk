29f09dfc4e51c375925f168af53a0ac13c01d788
doug chang <dougc@stanfordalumni.org>
2023-04-24 07:44:11 -0700

    canvas draw

---
+++ js/modules/draw_canvas/app.js
@@ -0,0 +1,8 @@
+const parent = document.getElementById("graphdiv")
+
+
+const canvas_element = document.createElement("canvas")
+parent.appendChild(canvas_element)
+console.log(canvas_element.getBoundingClientRect())
+
+
