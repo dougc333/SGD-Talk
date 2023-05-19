c3b6f27af00453862dd4eda783b8c18b738bdf08
doug chang <dougc@stanfordalumni.org>
2023-05-19 06:21:01 -0700

    material ui

---
+++ nextjs_practice/simple1/pages/a.js
@@ -0,0 +1,12 @@
+import {Button} from "@mui/material"
+
+
+export default function a(){
+    return(
+        <>
+        <Button variant="text">hi</Button>
+        <Button variant="contained">hello</Button>
+        <Button variant="outlined"> bye</Button>
+        </>
+    )
+}
--- nextjs_practice/simple1/pages/hi.js
+++ nextjs_practice/simple1/pages/hi.js
@@ -1,5 +1,24 @@
+import {
+    Grid
+  } from '@material-ui/core';
+
 
 
 export default function hi(){
-    return <h1>hi</h1>
+return(
+<Grid container spacing={2}>
+  <Grid item xs={8}>
+   <>aa</>
+  </Grid>
+  <Grid item xs={4}>
+   <>bb</>
+  </Grid>
+  <Grid item xs={4}>
+    <>cc</>
+  </Grid>
+  <Grid item xs={8}>
+    <>dd</>
+  </Grid>
+</Grid>
+)
 }
