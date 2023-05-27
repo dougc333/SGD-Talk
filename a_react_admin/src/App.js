d121c9846abfc6f9c6712abca84734299ea534e1
doug chang <dougc@stanfordalumni.org>
2023-05-27 08:19:33 -0700

    admin pages

--- a_react_admin/src/App.js
+++ a_react_admin/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext,useMode } from "./theme";
+import { ThemeProvider,CssBaseline } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode()
+  
   return (
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme = {theme}>
+      <CssBaseline />
     <div className="App">
-     
+      
+      <Topbar></Topbar>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
