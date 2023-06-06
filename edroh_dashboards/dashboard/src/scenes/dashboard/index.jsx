cd553ff8e76b3e6cb4ecc4673ae56d3274c46607
doug chang <dougc@stanfordalumni.org>
2023-06-05 19:03:12 -0700

    dashboard forms

--- edroh_dashboards/dashboard/src/scenes/dashboard/index.jsx
+++ edroh_dashboards/dashboard/src/scenes/dashboard/index.jsx
@@ -1,9 +1,14 @@
-
+import {Box} from "@mui/material"
+import Header from "../../components/Header"
 
 const Dashboard = ()=>{
-    return (
-        <div>Dashboard</div>
-    )
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="dashboard stuff here"></Header>
+      </Box>
+    </Box>
+  )
 }
 
 export default Dashboard
