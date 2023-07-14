a5784673b7ec485b73b263baa37f5df96d6bab2f
doug chang <dougc@stanfordalumni.org>
2023-07-14 13:59:16 -0700

    redo

--- flask/ch3/microblog/app/forms.py
+++ flask/ch3/microblog/app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired])
-    password = StringField('Password', validators = [DataRequired])
+    username = StringField('Username', validators = [DataRequired()])
+    password = StringField('Password', validators = [DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField("Sign In")
     
