09f2bd7396a56129d90d66792eff56f478f3c424
doug chang <dougc@stanfordalumni.org>
2023-07-16 16:40:44 -0700

    ch3 completed

--- flask/ch3/microblog/app/routes.py
+++ flask/ch3/microblog/app/routes.py
@@ -1,6 +1,6 @@
 
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 loggedin_user = {"username":'bob'}
@@ -21,17 +21,27 @@ posts = [
 
 ]
 
-
+#this isnt really correct we should do a redirect
 @app.route('/')
+@app.route("/index")
 def index():
-    print("calling index")
-    return render_template('index.html',posts=posts, loggedin_user=loggedin_user)
+    return render_template('index.html')
 
-@app.route('/login')
+@app.route("/index2")
+def index2():
+    return render_template('index2.html',posts=posts, loggedin_user=loggedin_user)
+
+@app.route('/login',methods=['GET','POST'])
 def login():
+    print("login form")
     form = LoginForm()
+    if form.validate_on_submit():
+        print("validate on submit login",form.username.data, form.remember_me.data)
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    #cafeful logged_in user requires session. 
     return render_template('login.html',title='Sign In', form=form, loggedin_user=loggedin_user)
 
-@app.route('loops')
+@app.route('/loops')
 def l():
     return render_template("index2.html",title="Display all posts", posts=posts, loggedin_user=loggedin_user)
