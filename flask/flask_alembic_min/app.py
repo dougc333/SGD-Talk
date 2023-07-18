c2fc9c245eddd450e9456e446bb3d82ee06ffa5f
doug chang <dougc@stanfordalumni.org>
2023-07-18 10:25:21 -0700

    min flask alembic without webforms

---
+++ flask/flask_alembic_min/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email=db.Column(db.String(255))
+    age = db.Column(db.Integer)
+
+
+
