9a7e7809650397acbceb562ff63b40076b04a32c
doug chang <dougc@stanfordalumni.org>
2023-07-18 09:50:30 -0700

    alembic

--- flask/ch4/microblog/app/models.py
+++ flask/ch4/microblog/app/models.py
@@ -15,5 +15,6 @@ class Post(db.Model):
   timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
   def __repr__(self):
     return '<Post {}>'.format(self.body)
