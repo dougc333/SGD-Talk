e2a45e27f95ed02f76a0309e9aaaf5993691c4f8
doug chang <dougc@stanfordalumni.org>
2023-07-18 17:17:58 -0700

    ch4

--- flask/flask_alembic_min/app.py
+++ flask/flask_alembic_min/app.py
@@ -12,7 +12,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     email=db.Column(db.String(255))
-    age = db.Column(db.Integer)
+
+class Posts(db.Model):
+    
 
 @app.route("/")
 def show():
@@ -21,4 +23,27 @@ def show():
     for u in users:
         print("username:",u.username)
     return render_template('index.html', users=users)
+
+@app.route("/addUsers")
+def addUsers():
+  u1 = User(username="user1", email="user1@wxample.com")
+  u2 = User(username="user2", email="user2@wxample.com")
+  u3 = User(username="user3", email="user3@wxample.com")
+  u4 = User(username="user4", email="user4@wxample.com")
+    
+  db.session.add(u1)
+  db.session.add(u2)
+  db.session.add(u3)
+  db.session.add(u4)
+  db.session.commit()
+  #need return 
+  users = User.query.all()
+  return render_template('index.html', users=users)
+
+#@app.route("/addPosts")
+#def addPosts():
+#    user1Posts = []
+    
+    
+
 
