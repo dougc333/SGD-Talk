c098d3f6d44f80aa8a35a64bfc04aa9e93d525d2
doug chang <dougc@stanfordalumni.org>
2023-08-31 15:05:32 -0700

    add

--- tanstack_table_vite/react_vite/src/component/BasicTable.jsx
+++ tanstack_table_vite/react_vite/src/component/BasicTable.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import {useReactTable} from '@tanstack/react-table'
+import {useReactTable, flexRender,getCoreRowModel} from '@tanstack/react-table'
 import dataJSON from './MOCK_DATA.json'
 import {columnDef} from './columns'
+import './table.css'
 
 
 const BasicTable = ()=>{
+  const finalData = React.useMemo(()=>(dataJSON,[]))
+  const finalColumnDef = React.useMemo(()=>(columnDef, []))
+
+
   const tableInstance = useReactTable({
     columns: columnDef,
     data:dataJSON,
+    getCoreRowModel: getCoreRowModel()
   })
 
   console.log(tableInstance.getHeaderGroups())
@@ -15,8 +21,43 @@ const BasicTable = ()=>{
   return(
     <table>
       <thead>
-        {/* {tableInstance.getHeaderGroups().map()} */}
+      {tableInstance.getHeaderGroups().map((headerEl) => {
+            return (
+              <tr key={headerEl.id}>
+                {headerEl.headers.map((columnEl) => {
+                  return (
+                    <th key={columnEl.id} colSpan={columnEl.colSpan}>
+                      {columnEl.isPlaceholder
+                        ? null
+                        : flexRender(
+                            columnEl.column.columnDef.header,
+                            columnEl.getContext()
+                          )}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
       </thead>
+      <tbody>
+      {tableInstance.getRowModel().rows.map((rowEl) => {
+            return (
+              <tr key={rowEl.id}>
+                {rowEl.getVisibleCells().map((cellEl) => {
+                  return (
+                    <td key={cellEl.id}>
+                      {flexRender(
+                        cellEl.column.columnDef.cell,
+                        cellEl.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+      </tbody>
     </table>     
   )
 }
