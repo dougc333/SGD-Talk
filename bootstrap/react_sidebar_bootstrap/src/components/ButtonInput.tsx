c5ecd017eadb700c27448dee5c44ad84f561c0a0
doug chang <dougc@stanfordalumni.org>
2023-09-06 16:29:42 -0700

    add

---
+++ bootstrap/react_sidebar_bootstrap/src/components/ButtonInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ButtonInput() {
+  return (
+    <div>ButtonInput</div>
+  )
+}
+
+export default ButtonInput
---
+++ bootstrap/react_sidebar_bootstrap/src/components/Sidebar3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar3() {
+  return (
+    <div>Sidebar3</div>
+  )
+}
+
+export default Sidebar3
